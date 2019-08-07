5663da2b00aac078a286286543459f0a3a9a6807
Adam St. Amand <adam.stamand@gmail.com>
2019-08-07 00:28:22 -0700

    Almost completed initial functionality

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,12 +1,28 @@
-function openMenu(){
+$(document).ready(function(){
+  $('.geopattern').each(function(){
+    $(this).geopattern($(this).data('pattern-id'));
+  });
+});
 
-
+function toggleForked(){
+  if ($('.fork-switch').prop('checked')) {
+    $('div[id=forked]').removeClass('hidden');
+  } else {
+    $('div[id=forked]').addClass('hidden');
+  }
 }
 
-function copyToClipboard(){
-
-
-
+function copyToClipboard(text){
+  var dummy = document.createElement("textarea");
+  // to avoid breaking orgain page when copying more words
+  // cant copy when adding below this code
+  // dummy.style.display = 'none'
+  document.body.appendChild(dummy);
+  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
 }
 
 
