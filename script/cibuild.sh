12917a148b879e55bb644102fbabb0ee632e7fbe
Adam St. Amand <adam.stamand@gmail.com>
2019-08-20 01:35:36 -0700

    specified ./site directory for htmlproofer

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer --allow-hash-href --url-ignore "https://angel.co/adam-stamand,https://www.linkedin.com/in/adam-st-amand/,/#.*/"
+bundle exec htmlproofer --allow-hash-href ./_site --url-ignore "https://angel.co/adam-stamand,https://www.linkedin.com/in/adam-st-amand/,/#.*/"
